bba4cb8fbb952b652d5cb8cc698b8b6a27b29eb3
Arthur Barata <tucosouza180@gmail.com>
2022-08-17 16:33:26 -0300

    new

--- src/App.js
+++ src/App.js
@@ -55,8 +55,7 @@ function App() {
           <CarouselImage></CarouselImage>
           <p className='p_apresentation'> Abrace a esperança você também!
             Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
-             Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,
-              discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+             Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.
               </p>
         </section>  
       </div>
@@ -76,29 +75,28 @@ function App() {
     data-aos-duration="1000">
         <div className='projects_div'>
           <h2 id="section_three">Ela prometeu ela cumpriu!</h2>
-           <p className='p_projects'>1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" The Extremes of Good and Evil by Cicero,
-             written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance . 
-             The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-             comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-              </p>
+           <p className='p_projects'>De Finibus Bonorum et Malorum" The Extremes of Good and Evil by Cicero.Por isso eu conto com o seu apoio.
+           Abrace a esperança você também!</p>
           <div className='video_um' data-aos='fade-right'>
               <video controls src='../assets/joana_video.mp4'></video>      
           </div> 
           <div className='carousel_videos'></div>
         </div>
     </section>
-    <section className='input_section'>
 
-      <h2>Abraçe a causa!</h2>
-      <h4>Faça parte do nosso trabalho.</h4>
+    <section className='input_section'>
+      <div className='input_title'>
+        <h2>Abraçe a causa!</h2>
+        <h4>Faça parte do nosso trabalho.</h4>
+        <p>A caminhada até aqui foi longa, mas não caminhei sozinha. Sempre tive ao meu lado, 
+          pessoas que acreditam no meu trabalho 
+          e sabem que juntos podemos fazer a diferença. Por isso eu conto com o seu apoio.
+           Abrace a esperança você também!</p>
+      </div>
     </section>
     
     
   </div>
-
-
-
-
  
   )
 }
