5de7f9118fd8bfb6509eb8918606fe5bc345aca2
Arthur Barata <tucosouza180@gmail.com>
2022-08-21 01:52:37 -0300

    input atualizado

--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,78 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import InputMask from 'react-input-mask';
 
 
 function Form(){
+  
 
-  const[val,setVal]=useState(<input id='name' type='text' name='input_name' placeholder='WhatsApp'></input>)
+  const[input,setInput]=useState(<input id='name' type='text' name='input_name' placeholder='WhatsApp'></input>)
+  const[val,setVal]=useState('')
+  const[cidades,setCidades]=useState(['MANAUS','PARINTIS', 'ITACOATIARA','ALVARAES',
+  "AMATURA",
+  "ANAMA",
+  'ANORI',
+  'APUI',
+  'ATALAIA DO NORTE',
+  'AUTAZES',
+  'BARCELOS',
+  'BARREIRINHA',
+  'BENJAMIN CONSTANT',
+  'BERURI',
+  'BOA VISTA DO RAMOS',
+  'BOCA DO ACRE',
+  'BORBA',
+  'CAAPIRANGA',
+  'CANUTAMA',
+  'CARAUARI',
+  'CAREIRO',
+  'CAREIRO DA VARZEA',
+  'COARI',
+  'CODAJAS',
+  'EIRUNEPE',
+  'ENVIRA',
+  'FONTE BOA', 
+  'GUAJARA',
+  'HUMAITA',
+  'IPIXUNA',
+  'IRANDUBA',
+  'ITAMARATI',
+  'ITAPIRANGA',
+  'JAPURA',
+  'JURUA',
+  'JUTAI',
+  'LABREA',
+  'MANACAPURU',
+  'MANAQUIRI',
+  'MANICORE',
+  'MARAA',
+  'MAUES',
+  'NHAMUNDA',
+  'NOVA OLINDA DO NORTE',
+  'NOVO AIRAO',
+  'NOVO ARIPUANA',
+  'PAUINI',
+  'PRESIDENTE FIGUEIREDO',
+  'RIO PRETO DA EVA',
+  'SANTA ISABEL DO RIO NEGRO',
+  'SANTO ANTONIO DO ICA',
+  'SAO GABRIEL DA CACHOEIRA',
+  'SAO PAULO DE OLIVENCA',
+  'SAO SEBASTIAO DO UATUMA',
+  'SILVES',
+  'TABATINGA',
+  'TAPAUA',
+  'TEFE',
+  'TONANTINS',
+  'UARINI',
+  'URUCARA',
+  'URUCURITUBA' 
+  ])
 
-    function change(){
-      setVal(<InputMask mask="(99)99999-9999 "  maskChar="_" id='inputtel' alwaysShowMask='true'></InputMask>)
+    function change(event){
+      setInput(<InputMask mask="(99)99999-9999 "  maskChar="_" id='inputtel' alwaysShowMask='true'></InputMask>)
+       setVal(event.target.value)
     }
-    function changeb(){
-      setVal(<input id='name' type='text' name='input_name' placeholder='WhatsApp'></input>)
-    }
+ 
   
 
   return(
@@ -26,11 +87,16 @@ function Form(){
       </div>
       <form className='form' >
         <input id='input_name' type='text' name='input_name' placeholder='Nome'></input>
-      <div id='input_zap' onFocus={change} onBlur={changeb}> 
-        {val}
+      <div id='input_zap' onFocus={change} > 
+        {input}
       </div>
       <input type='email' name='email' placeholder='E-mail' autoComplete='on'></input>
+      <select>
+        {cidades.map((cid)=>{
+         return <option key={cid} value={cid}>{cid}</option>
+        })}
       
+      </select>
       </form>
     </section>)
 }  
